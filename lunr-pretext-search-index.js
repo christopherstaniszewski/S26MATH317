e5514321162bfdae966cd83bc4d868e5f8402a2e
christopherstaniszewski <christopherstaniszewskiwork@gmail.com>
2026-04-06 12:01:22 -0400

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,68 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Christopher Staniszewski Mathematics Framingham State University  Last Updated:        "
+},
+{
+  "id": "information",
+  "level": "1",
+  "url": "information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Course Information",
+  "body": " Course Information     Instructor  Christopher Staniszewski, Ph. D. (he\/him), HH 402C, cstaniszewski@framingham.edu .    Student Help Hours  Mondays, Thursdays, and Fridays at 11:30-12:30 and\/or by appointment    Class meets  MW 12:30-1:20, Th 12:30-2:20 in Hemenway 111    Course Description  An exploration of numerical, graphical, and symbolic approaches to function concepts with emphasis on real-world applications, modeling, and problem-solving skills. Topics include polynomial and rational functions, equations and inequalities, systems of linear equations, and the connection between functions and their graphs.    Prerequisite  Satisfactory score on the mathematics placement. Also, our course is a prerequisite other courses, including MATH 180 Precalculus. Some of them require you to earn at least a C in MATH 123.    Homework  All homework will appear in MyOpenMath .    Textbook   College Algebra by OpenStax (5th edition). Read Online    Reading Mathematics   Don’t just read it; fight it! Ask your own questions, look for your own examples, discover your own proofs. Is the hypothesis necessary? Is the converse true? What happens in the classical special case? What about the degenerate cases? Where does the proof use the hypothesis?   Paul Halmos      "
+},
+{
+  "id": "Grading",
+  "level": "1",
+  "url": "Grading.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Grading",
+  "body": " Grading   Homework  Homework for this course will be submitted through the online system MyOpenMath. To create an account and enroll in our course, go to MyOpenMath and either log in using your existing account, or create a new account by clicking on `Register as a new student.' Use the following information to find our course:   Course Name:  S26 Introduction to Functions    Course ID:  315120    Enrollment Key:  s26math   Homework assignments can be worked on past the due date by using a ``Late Pass.'' Work done under a Late Pass will receive a minor grade deduction. All homework assignments, even those done using a Late Pass, must be completed by the last day of classes.    Quizzes\/Classwork\/Assignments\/Projects  No makeup assignments here, but I will drop your lowest two quiz scores.    Exams  We will have three in-semester exams held on the following dates.   Monday, February 23rd    Monday, March 30th    Monday, April 27th       Final Exam  Our cumulative final exam will be held Monday, May 11th from 8:00am-11:00am in Hemenway Hall 405.    Final Grades  The following percentage weights are used for calculating your weighted course average.    Category  Weight    Homework  10%    Quizzes  20%    Exams  40%    Final Exam  30%    Final grades will be assigned based on the following scale.    Percentage  Grade    93-100  A    90-93  A-    87-90  B+    83-87  B    80-83  B-    77-80  C+    73-77  C    70-73  C-    67-70  D+    63-67  D    60-63  D-    Below 60  F     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Course Policies",
+  "body": " Course Policies   Make sure you familiarize yourself with the following course and university policies.    Attendance  Regular attendance is an expectation for all students in this course because attendance is important for learning the material. But attendance means more than simply physical presence. It also requires mental presence. Attend every class awake, on time, and ready to learn with cell phones and all other electronic devices off and out of sight.  In the event of absence, students are responsible for all material and announcements discussed in class.    Calculators  Calculators and other electronic devices are not allowed on quizzes or exams.    Makeup Exams  Makeup exams may be given for students who miss an exam due to an emergency or to an approved scheduling conflict (see below). Makeup exams must be scheduled after the original exam, and will be administered as soon as is reasonably feasible. If you miss any exam due to illness or emergency, you must contact me within 24 hours. If you know that you will need to miss an examination due to religious observances or University-sanctioned events, then you must contact your instructor at least 48 hours before the relevant evaluation. In all cases, absences must be documented. Failure to provide documentation for an absence will result in a grade of zero for the exam. Exceptions will also be made to comply with the American's with Disabilities Act (ADA) and Title IX.    My Expectations     Show an attitude of respect toward everyone in the classroom. Racist, sexist, antisemitic, Islamophobic, or pornographic content is never allowed.    Complete homework assignments carefully, thoughtfully, and on time.    Prepare for assessments successfully. Make sure you test yourself before I do!    Attend help sessions and form study groups for extra help.    For each credit hour spend at least two hours working on the material outside of class. For us that means at least eight hours of work per week outside of class.       Academic Integrity  Integrity is essential to academic life. Consequently, students who enroll at Framingham State University agree to maintain high standards of academic honesty and scholarly practice. They shall be responsible for familiarizing themselves with the published policies and procedures regarding academic honesty. Common infractions of the Policy on Academic Honesty in mathematics courses include, but are not limited to:   Plagiarism: claiming as one's own work the published or unpublished literal or paraphrased work of another. It should be recognized that plagiarism is not only academically dishonest but also illegal;     Use of dishonest procedures in computer, laboratory, studio, or field work;    Cheating on exams, tests, quizzes, assignments, and papers, including the giving or acceptance of these materials and other sources of information without the permission of the instructor(s);    Unauthorized collaboration with other individuals in the preparation of course assignments;       Generative AI   Use of Generative AI is prohibited on all assessments. You may use your best judgement in terms of using AI to study. For instance, you may create study guides or ask for help with a particular step of a textbook exercise.    Antiracism at FSU  At Framingham State University, faculty, staff, and students work together to sustain a learning, working, and living community free from hate, discrimination, harassment, and intolerance. We recognize the damaging effects of systemic racism - including policies, structures, and historic practices - on the experience and success of communities of color. Coming from different backgrounds and different levels of privilege, we can all affirm and engage in antiracist work.  Diversity of voices, and of minds, strengthens our ability to solve problems and to ask and answer questions about the world we share. As your instructor, I commit to upholding community values of inclusion, civility, accessibility, and mutual respect. I expect this class to commit to creating a community that affirms and welcomes all persons from diverse backgrounds and experiences, and supports the realization of everyone's potential.    Sexual Harassment  Framingham State University is committed to maintaining a safe and healthy learning, living and working environment that is free from all forms of sexual violence, sexual harassment, gender-based harassment, domestic violence, dating violence, stalking and retaliation. If you or someone you know has experienced any form of sexual or gender-based misconduct, we urge you to visit the SHAPE website for information regarding on-campus and community resources, University policy, contact information for the Title IX Coordinators, confidential and non-confidential reporting options, victim rights, and more.    Notice of Non-Discrimination and Diversity  Framingham State University is committed to a policy of non-discrimination, equal opportunity, diversity, and affirmative action. The University is dedicated to providing educational, working, and living environments that value the diverse backgrounds of all people. Furthermore, the Massachusetts Civil Rights Act (``MCRA,'' M.G.L. c. 12, §§ 11H, 11I, 11J) protects the rights of all residents of and visitors to Massachusetts to be free from bias-motivated threats, intimidation, and coercion that interfere with their civil rights. The MCRA protects the right to attend school, live peacefully, and enjoy other basic rights.    Syllabus Changes  The instructor reserves the right to make changes to this syllabus. Any changes will be announced to the class in a timely manner.   "
+},
+{
+  "id": "FAQ",
+  "level": "1",
+  "url": "FAQ.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Frequently Asked Questions",
+  "body": " Frequently Asked Questions   How can I get extra help learning the material?  I encourage you to come to Student Help Hours whenever you have questions. Make sure to see me as soon as possible if you feel that you are falling behind.   CASA also provides math tutoring. Follow the link for their tutoring schedule.    What if I need academic accommodations like extra time or a reduced distraction environment for exams?  Any student eligible for and requesting academic accommodation due to a disability is requested to provide Documentation Confirmation from Disability\/Access Services within the first two weeks of the beginning of class. Disability\/Access Services is located in the Center for Academic Success and Achievement (CASA). Please email disabilityservices@framingham.edu if you have questions or if you need to schedule an appointment.    What if I am struggling with food or housing insecurity?  Visit the Rams Resource Center in West Hall 105 for non-perishable food items and toiletries.  To support students who are facing temporary or chronic homelessness and\/or difficulty affording food, we have established a Single Point of Contact (SPOC) staff person to assist students with navigating campus and local resources. If you or someone you know needs help, please contact Olivia Fenty at (508) 626-4767, ofenty@framingham.edu , or stop by the Dean of Students Office. The Dean of Students Office is located on the 5th floor of the McCarthy Center in Suite 504.    What other help is available on campus?      Health Center (Foster Hall)     Counseling Center (Foster Hall)     Dean of Students (Office in McCarthy 504)     Student Support Resources       "
+},
+{
+  "id": "important-dates",
+  "level": "1",
+  "url": "important-dates.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Important Dates",
+  "body": " Important Dates  The following are the most significant university and course dates this term. For smaller course assignment due dates, check the Canvas and MyOpenMath calendars regularly.     Tuesday, Jan 27  Last Day to Add\/Drop a Course    Monday, Feb 16  Holiday: No University Classes    Wednesday, Feb 25  Exam 1    March 14 - March 22  Spring Vacation (No Classes)    Monday, Mar 30  Exam 2    Monday, Apr 20  Holiday: No University Classes    Friday, Apr 24  Last Day to Withdraw with a W  Last Day to Take a Course Credit\/No Credit    Monday, Apr 27  Exam 3    Friday, May 08  Semester Classes End    Monday, May 11  Final Exam from 8am to 11am     "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
